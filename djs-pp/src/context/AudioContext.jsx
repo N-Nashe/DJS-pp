110b5d2e63e411328ed3f90755dc3ce55b55f408
N-Nashe <munalechakona@gmail.com>
2025-10-17 12:55:55 +0200

    correct AudioProvider component definition and audio state management added

--- djs-pp/src/context/AudioContext.jsx
+++ djs-pp/src/context/AudioContext.jsx
@@ -12,3 +12,36 @@ export const useAudioContext = () =>{
     
     } return audioContextValue;
 }
+
+//Audio provider component puts audio state in context
+
+export const AudoProvider =({children}) =>{
+        
+        // points to the html audio element
+    
+        const audioRef = useRef(null)
+//audio state management
+        const [currentEpisode, setCurrentEpisode] = useState(null);
+        const [isPlaying, setIsPlaying] =useState(false);
+        const [currentTime, setCurrentTime] = useState(0);
+        const [duration, setDuration]= useState(0);
+        const [isLoading, setIsLoading]= useState(false);
+
+        //function to load and play episode
+
+
+        //values for use by other components
+        const audioValues ={
+            currentEpisode,
+            isPlaying,
+            currentTime,
+            duration,
+            isLoading,
+        }
+
+        return(
+            <AudioContext.Provider value={audioValues}>
+                {children}
+            </AudioContext.Provider>
+        )
+}
