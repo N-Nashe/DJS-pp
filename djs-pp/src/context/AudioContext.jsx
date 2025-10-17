872627db3852d9a8932bdfe24be68cc783e5d924
N-Nashe <munalechakona@gmail.com>
2025-10-17 13:38:09 +0200

    implement play, pause, and toggle functionality in AudioProvider

--- djs-pp/src/context/AudioContext.jsx
+++ djs-pp/src/context/AudioContext.jsx
@@ -28,6 +28,50 @@ export const AudoProvider =({children}) =>{
         const [isLoading, setIsLoading]= useState(false);
 
         //function to load and play episode
+        const playEpisode = async(episode) =>{
+            try{
+                setIsLoading(true);
+
+                const audioUrl = await fetchPodcastAudio(episode.file);
+
+                setCurrentEpisode(episode);
+
+                if(!audioRef.current){
+                    audioRef.current new Audio();
+                }
+
+                //set audio source and play
+                audioRef.current.src = audioUrl
+                await audioRef.current.play()
+
+                setIsLoading(false)
+            } catch (error){
+                console.error('Error playing episode:', error);
+                setIsLoading(false);
+            }
+
+            //function to pause audio
+            const pauseAudio =() =>{
+                if (audioRef.current){
+                    audioRef.current.pause();
+                    setIsPlaying(false);
+                }
+            }
+
+            //function to toggle play/pause
+            const togglePlayPause = () =>{
+                if (isPlaying){
+                    pauseEpisode();
+
+                } else if (currentEpisode && audioRef.current){
+                    audioRef.current.play();
+                    setIsPlaying(true);
+                }
+            }
+
+               
+            }
+        }
 
 
         //values for use by other components
@@ -37,6 +81,12 @@ export const AudoProvider =({children}) =>{
             currentTime,
             duration,
             isLoading,
+
+            //calling functions
+            playEpisode,
+            pauseEpisode,
+            togglePlayPause,
+        
         }
 
         return(
